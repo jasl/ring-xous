86c1929d12b73eadb50084399a62b8169cb12fb1
jasl <jasl9187@hotmail.com>
2023-05-12 07:48:26 +0800

    Support c2rust for multi-arch

--- ring-transpile-c2rust.py
+++ ring-transpile-c2rust.py
@@ -38,7 +38,25 @@ RING_C_FILES = [
     "crypto/fipsmodule/ec_17/p256.c",
 ]
 
-COMMANDS_FILE = "compile_commands.json"
+TARGETS = [
+    {
+        "target": "riscv32imac-unknown-xous-elf",
+        "compile_arguments": [
+            "-D__xous__",
+            "-D__riscv",
+            "-D__riscv_xlen=32",
+            "-m32"
+        ],
+        "save_to_dir": "src/c2rust/xous"
+    },
+    {
+        "target": "wasm32-unknown-unknown",
+        "compile_arguments": [
+            "-m32"
+        ],
+        "save_to_dir": "src/c2rust/wasm32"
+    }
+]
 
 p_sizeof = re.compile(r'(.*)(std::mem::size_of::)(.*)(as u64)(.*)')
 
@@ -106,10 +124,10 @@ def massage_line(line):
     return line
 
 
-def lint():
+def lint(work_dir, target):
     # lint the c2rust using cargo and a cleanup pass
     build = subprocess.run(
-        ["cargo", "build", "--target=riscv32imac-unknown-xous-elf"],
+        ["cargo", "build", f"--target={target}"],
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     state = "SEARCHING"
     subs = {}
@@ -152,7 +170,7 @@ def lint():
                 warntype = ""
 
     for fname in subs.keys():
-        if 'src/c2rust' in fname:
+        if work_dir in fname:
             # print(fname)
             # print(subs[fname])
             with open(fname, "r") as src_file:
@@ -182,11 +200,10 @@ def lint():
                     line_no += 1
                     print(line, file=dst_file, end="")
 
-
-def run():
+def generate_c2rust_files_list(output_file_name, append_arguments: []):
     # Generate the `compile_commands.json` file that c2rust uses
     cwd = os.getcwd()
-    with open(COMMANDS_FILE, "w") as cmd_file:
+    with open(output_file_name, "w") as cmd_file:
         files_list = []
         for file in RING_C_FILES:
             rs_file = file.replace(".c", ".rs")
@@ -201,13 +218,10 @@ def run():
                     "-c",
                     "-o",
                     "build/tmp.o",
-                    "-m32",
                     "-Iinclude",
                     "-UOPENSSL_X86_64",
                     "-U__x86_64",
-                    "-D__xous__",
-                    "-D__riscv",
-                    "-D__riscv_xlen=32",
+                    *append_arguments,
                     file
                 ]
             }
@@ -215,40 +229,54 @@ def run():
 
         print(json.dumps(files_list, indent=4), file=cmd_file)
 
-    subprocess.run(["c2rust", "transpile", COMMANDS_FILE])
+def run():
+    for target_entry in TARGETS:
+        target = target_entry["target"]
+        append_arguments = target_entry["compile_arguments"]
+        save_to_dir = target_entry["save_to_dir"]
 
-    if not os.path.exists("src/c2rust"):
-        os.mkdir("src/c2rust")
+        print(f"Transpiling for {target}")
 
-    print("Add this to the end of `src/lib.rs`:")
+        # output_file_name = f"compile_commands.{target}.json"
+        output_file_name = "compile_commands.json"  # c2rust only recognize `compile_commands.json` others will error
+        generate_c2rust_files_list(output_file_name, append_arguments)
+        subprocess.run(["c2rust", "transpile", output_file_name])
 
-    print("mod c2rust {")
-    for file in RING_C_FILES:
-        mod_name = file.split("/")[-1].split(".")[0]
-        rs_file = file.replace(".c", ".rs")
-        # print(f"    #[path = \"../{rs_file}\"]")
-        print(f"    mod {mod_name};")
-        with open(rs_file, "r") as src_file:
-            with open(f"src/c2rust/{mod_name}.rs", "w") as dest_file:
-                print("#![allow(non_camel_case_types)]", file=dest_file)
-                print("#![allow(non_snake_case)]", file=dest_file)
-                print("#![allow(non_upper_case_globals)]", file=dest_file)
-                # print("use core::ffi::*;", file=dest_file)
-                for line in src_file:
-                    print(massage_line(line), file=dest_file)
-            subprocess.run(["rm", rs_file])
-            subprocess.run(["rustfmt", "src/c2rust/{}.rs".format(mod_name)])
-    print("}")
-    # multiple passes of linting are needed to tease out all the unused mut warnings
-    # each pass removes some muts from the warning tree that propagates backwards...
-    # we don't loop this but make it individual calls because the depth of this sort of depends
-    # upon the code itself.
-    print("linting...")
-    lint()
-    print("pass2")
-    lint()
-    print("pass3")
-    lint()
+        if not os.path.exists(save_to_dir):
+            os.makedirs(save_to_dir, exist_ok=True)
+
+        for file in RING_C_FILES:
+            mod_name = file.split("/")[-1].split(".")[0]
+            rs_file = file.replace(".c", ".rs")
+            with open(rs_file, "r") as src_file:
+                with open(f"{save_to_dir}/{mod_name}.rs", "w") as dest_file:
+                    print("#![allow(non_camel_case_types)]", file=dest_file)
+                    print("#![allow(non_snake_case)]", file=dest_file)
+                    print("#![allow(non_upper_case_globals)]", file=dest_file)
+                    # print("use core::ffi::*;", file=dest_file)
+                    for line in src_file:
+                        print(massage_line(line), file=dest_file)
+                subprocess.run(["rm", rs_file])
+                subprocess.run(["rustfmt", f"{save_to_dir}/{mod_name}.rs"])
+        # multiple passes of linting are needed to tease out all the unused mut warnings
+        # each pass removes some muts from the warning tree that propagates backwards...
+        # we don't loop this but make it individual calls because the depth of this sort of depends
+        # upon the code itself.
+        for i in range(3):
+            print(f"Linting...iter: {i}")
+            lint(save_to_dir, target)
+
+        print("Add this to the end of `src/lib.rs`:")
+
+        print(f"#[cfg(target=\"{target}\")]")
+
+        print("mod c2rust {")
+        for file in RING_C_FILES:
+            mod_name = file.split("/")[-1].split(".")[0]
+            relative_path = save_to_dir.split("/")[-1]
+            print(f"    #[path = \"{relative_path}/{mod_name}.rs\"]")
+            print(f"    mod {mod_name};")
+        print("}")
 
 
 if __name__ == "__main__":
